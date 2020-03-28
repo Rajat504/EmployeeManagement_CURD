980ee1889cf5cb2a97100707bb495e4c0ae00fbb
Rajat504 <rajatravindar504@gmail.com>
2020-03-29 00:31:43 +0530

    [Rajat] Add. added a EmployeeControllerClass To Route Data

---
+++ EmployeeMangementSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,77 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmployeeController.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Rajat Ravindar"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace EmployeeMangementCurd_Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Manager.ManagerInterface;
+    using Microsoft.AspNetCore.Mvc;
+    using Model;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        public readonly IEmployeeManager manager;
+        public EmployeeController(IEmployeeManager manager)
+        {
+            this.manager = manager;
+        }
+        
+        [Route("AddEmployee")]
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee employee)
+        {
+            var result = await this.manager.AddEmployee(employee);
+            if (result == 1)
+            {
+                return this.Ok(employee);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+
+        [Route("GetAllEmployee")]
+        [HttpGet]
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return this.manager.GetAllEmployees();
+        }
+
+        [Route("UpdateEmployee")]
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(Employee employeeChanges)
+        {
+            var result = await this.manager.UpdateEmployee(employeeChanges);
+            if (result == 1)
+            {
+                return this.Ok(employeeChanges);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        public Employee DeleteEmployee(int id)
+        {
+            return this.manager.DeleteEmployee(id);
+        }
+
+        [Route("GetEmployee")]
+        [HttpGet]
+        public Employee GetEmployee(int id)
+        {
+            return this.manager.GetEmployee(id);
+        }
+    }
+}

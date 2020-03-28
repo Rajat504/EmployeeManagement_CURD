f1e3bfa677d65701728e4c5840502ac109f69b14
Rajat504 <rajatravindar504@gmail.com>
2020-03-29 00:36:15 +0530

    [Rajat] Add. Created a Project Name Model

---
+++ Model/Program.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+// Copyright © 2020 Company="BridgeLabz".
+// </copyright>
+// <creator name="Shivam Dewangan"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Model
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

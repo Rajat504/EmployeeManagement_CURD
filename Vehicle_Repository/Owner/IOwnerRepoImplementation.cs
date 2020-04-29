08069fd1cbe1905f08e968ac020b52ddaff47b86
Rajat504 <rajatravindar504@gmail.com>
2020-04-29 09:46:10 +0530

    [Rajat]add.i have added owner repository implementation in vehicle repository

---
+++ Vehicle_Repository/Owner/IOwnerRepoImplementation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicle_Repository.Owner
+{
+  public class IOwnerRepoImplementation : IOwnerRepository
+  {
+        public readonly VehicleDBContext vehicleDBContext;
+        public IOwnerRepoImplementation(VehicleDBContext vehicleDBContext)
+        {
+            this.vehicleDBContext = vehicleDBContext;
+        }
+        public 
+
+
+  }
+}

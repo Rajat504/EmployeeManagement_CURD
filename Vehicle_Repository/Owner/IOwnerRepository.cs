d2b91dec0c7c705f8833ddd60009c841ae05d326
Rajat504 <rajatravindar504@gmail.com>
2020-04-29 08:31:55 +0530

    [Rajat]add.i have added owner repository

---
+++ Vehicle_Repository/Owner/IOwnerRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bogus.DataSets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Repository.Owner
+{
+   public interface IOwnerRepository
+   {
+        Task<int> AddParking(Vehicle vehicle);
+        Vehicle GetVehicle(int ParkingSlotNumber);
+        Vehicle RemoveParking(int ParkingSlotNumber);
+    }
+}

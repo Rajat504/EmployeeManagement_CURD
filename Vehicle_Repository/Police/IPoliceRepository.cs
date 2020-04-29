a07074453ff6e308ffc5e1ad7ddf2e9100e8ff41
Rajat504 <rajatravindar504@gmail.com>
2020-04-29 09:23:07 +0530

    [Rajat]add . i have added new repository for police and security

---
+++ Vehicle_Repository/Police/IPoliceRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bogus.DataSets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Repository.Police
+{
+   public interface IPoliceRepository
+   {
+        string GetParkingSpace();
+        IEnumerable<Vehicle> GetVehicles();
+        Task<int> AddParking(Vehicle vehicle);
+        Vehicle RemoveParking(int ParkingSlotNumber);
+        double ParkingCharge(int ParkingSlotNumber);
+   }
+}
---
+++ Vehicle_Repository/security/ISecurityRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bogus.DataSets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle_Repository.security
+{
+   public interface ISecurityRepository
+   {
+        string GetParkingSpace();
+        Task<int> AddParking(Vehicle vehicle);
+        IEnumerable<Vehicle> GetVehicles();
+        Vehicle GetVehicle(int ParkingSlotNumber);
+    }
+}

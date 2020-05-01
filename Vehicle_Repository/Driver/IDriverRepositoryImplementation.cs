ba7ebb5f14b0c531200f376dee57cfcc7d121388
Rajat504 <rajatravindar504@gmail.com>
2020-05-01 14:08:57 +0530

    [Rajat]add.i have refactor some changes in the model class

--- Vehicle_Repository/Driver/IDriverRepositoryImplementation.cs
+++ Vehicle_Repository/Driver/IDriverRepositoryImplementation.cs
@@ -29,55 +29,6 @@ namespace Vehicle_Repository.Driver
 
         }
 
-        public Vehicle GetVehicle(int ParkingSlotNumber)
-        {
-            return vehicleDBContext.vehicle.Find(ParkingSlotNumber);
-        }
-
-        public double ParkingCharge(int ParkingSlotNumber)
-        {
-            Vehicle vehicle = vehicleDBContext.vehicle.Find(ParkingSlotNumber);
-            DateTime Entrytime =vehicle.EntryTime;
-            DateTime ExitTime = vehicle.ExitTime;
-            double totalhour = (Entrytime - ExitTime).TotalHours;
-            if (vehicle.ParkingType.Equals("valletparking", StringComparison.InvariantCultureIgnoreCase)
-                && vehicle.drivertype.Equals("ParkingHour", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return totalhour * 0;
-            }
-            else if (vehicle.ParkingType.Equals("own", StringComparison.InvariantCultureIgnoreCase)
-                    && vehicle.drivertype.Equals("normal", StringComparison.InvariantCultureIgnoreCase))
-                 {
-                if (totalhour < 1)
-                {
-                    return 50;
-                }
-                if (vehicle.VehicleType.Equals("twowheeler", StringComparison.InvariantCultureIgnoreCase) && totalhour >= 1)
-                {
-                    return totalhour * vehicle.Chargeperhour;
-                }
-                else if (vehicle.VehicleType.Equals("fourwheeler", StringComparison.InvariantCultureIgnoreCase) && totalhour >= 1)
-                {
-                    return totalhour * vehicle.ChargePerHour;
-                }
-
-                 }
-            return 0;
-
-        }
-        public IEnumerable<Vehicle> GetVehicles()
-        {
-            return VehicleDBContext.vehicle;
-        }
-        Vehicle RemoveParking(int ParkinfSlotNumber)
-        {
-            Vehicle vehicle = vehicleDBContext.vehicle.find(ParkingSlotNumber);
-            if (vehicle != null)
-            {
-                vehicleDBContext.vehicle.Remove(vehicle);
-            }
-            vehiclelist.Remove(vehicle);
-            return vehicle;
-        }
+      
     }
 }
